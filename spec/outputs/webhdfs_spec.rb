8acbbb0b3d2a401076033943bf8891a9ff43f6c7
dstore-dbap <b.puttmann@dbap.de>
2015-05-21 17:30:11 +0200

    Added integration tests.

--- spec/outputs/webhdfs_spec.rb
+++ spec/outputs/webhdfs_spec.rb
@@ -1,16 +1,28 @@
 # encoding: utf-8
 require 'logstash/devutils/rspec/spec_helper'
 require 'logstash/outputs/webhdfs'
+require 'webhdfs'
 
 describe 'outputs/webhdfs' do
 
-  let(:event) do
-    LogStash::Event.new(
-        'message' => 'fanastic log entry',
-        'source' => 'someapp',
-        'type' => 'nginx',
+  webhdfs_server = 'localhost'
+  webhdfs_port = 50070
+  webhdfs_user = 'hadoop'
+  path_to_testlog = '/user/hadoop/test.log'
+  current_logfile_name = '/user/hadoop/test.log'
+
+  event = LogStash::Event.new(
+        'message' => 'Hello world!',
+        'source' => 'out of the blue',
+        'type' => 'generator',
+        'host' => 'localhost',
         '@timestamp' => LogStash::Timestamp.now)
-  end
+
+  config =  { 'server' => webhdfs_server + ':' + webhdfs_port.to_s,
+              'user' => webhdfs_user,
+              'path' => path_to_testlog }
+
+  client = WebHDFS::Client.new(webhdfs_server, webhdfs_port, webhdfs_user)
 
   context 'when initializing' do
 
@@ -24,13 +36,13 @@ describe 'outputs/webhdfs' do
       insist { configuration_error } == true
     end
 
-    subject = LogStash::Plugin.lookup("output", "webhdfs").new('server' => '127.0.0.1:50070', 'path' => '/path/to/webhdfs.file')
-
     it 'should register' do
+      subject = LogStash::Plugin.lookup("output", "webhdfs").new(config)
       expect { subject.register }.to_not raise_error
     end
 
     it 'should have default config values' do
+      subject = LogStash::Plugin.lookup("output", "webhdfs").new(config)
       insist { subject.idle_flush_time } == 1
       insist { subject.flush_size } == 500
       insist { subject.open_timeout } == 30
@@ -42,6 +54,36 @@ describe 'outputs/webhdfs' do
       insist { subject.snappy_bufsize } == 32768
       insist { subject.snappy_format } == 'stream'
       insist { subject.remove_at_timestamp } == true
+    end
+  end
+
+  context 'when writing messages' do
+    it 'should write 100 messages uncompressed' do
+      current_logfile_name = path_to_testlog
+      config['compression'] = 'none'
+      subject = LogStash::Plugin.lookup("output", "webhdfs").new(config)
+      subject.register
+      for _ in 0..99
+        subject.receive(event)
+      end
+      subject.teardown
+      insist { client.read(current_logfile_name).lines.count } == 100
+    end
+
+    it 'should write 100 messages gzip compressed' do
+      current_logfile_name = path_to_testlog + ".gz"
+      config['compression'] = 'gzip'
+      subject = LogStash::Plugin.lookup("output", "webhdfs").new(config)
+      subject.register
+      for _ in 0..99
+        subject.receive(event)
+      end
+      subject.teardown
+      insist { Zlib::Inflate.new(window_bits=47).inflate(client.read(current_logfile_name)).lines.count } == 100
+    end
+
+    after :each do
+      client.delete(current_logfile_name)
     end
 
   end
